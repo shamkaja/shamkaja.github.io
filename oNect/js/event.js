649b0501d08fd25a5044d560216602449fc39c8d
Kaja Najmudheen N <kajanaj@gmail.com>
2020-12-21 12:41:19 +0530

    Add files via upload

---
+++ oNect/js/event.js
@@ -0,0 +1,1 @@
+function bind(id,run){var effect=run();var btnBox=document.querySelector(id).nextElementSibling;btnBox.querySelector('[data-open]').onclick=function(){effect.open();};btnBox.querySelector('[data-pause]').onclick=function(){effect.pause();};}
