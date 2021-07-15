450adb38fe6b5badbdb6c7a3e1f4945f254f9c9a
Kaja Najmudheen N <kajanaj@gmail.com>
2021-07-15 20:51:38 +0530

    Add files via upload

---
+++ oflow/js/front_end.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+$(function () {
+  // Slideshow 1
+  $("#slider1").responsiveSlides({
+
+
+    nav: true,
+    speed: 500,
+    namespace: "centered-btns"
+  });
+
+});
+
+
+const leftButton = document.querySelector('.arrow.left');
+const rightButton = document.querySelector('.arrow.right');
+
+leftButton.addEventListener('click', () => {
+  turnSlider('right');
+});
+
+rightButton.addEventListener('click', () => {
+  turnSlider('left');
+});
+
+function turnSlider(direction) {
+  const slides = document.querySelectorAll(`.slide`);
+  slides.forEach(slide => {
+    let currentSlide = +(slide.classList + '').split('-')[1];
+    let slideToBe;
+    switch (direction) {
+      case 'left': {
+        slideToBe = currentSlide - 1;
+        if (slideToBe < 1) {
+          slideToBe = slides.length;
+          slide.style.left = '-500px';
+        }
+        break;
+      }
+      case 'right': {
+        slideToBe = currentSlide + 1;
+        if (slideToBe > slides.length) {
+          slideToBe = 1;
+          slide.style.left = '2000px';
+        }
+        break;
+      }
+    }
+    slide.classList.remove(`slide-${currentSlide}`);
+    slide.classList.add(`slide-${slideToBe}`);
+  });
+}
+
